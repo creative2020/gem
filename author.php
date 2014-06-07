8979a48d258d756c1d57cb18ee6130da1be3f68a
Jeff Potts <jeff@2020creative.com>
2014-06-07 16:55:20 -0500

    v1.16

--- author.php
+++ author.php
@@ -1,32 +1,104 @@
 <?php get_header(); ?>
-			
-<div id="slider" class="gem-slider">
-            slider img
-        </div> <!-- end #slider -->
-<div class="container">
-    <div id="content" class="row content clearfix col-md-12">
+
+<div id="party-header-wrap" class="row">
+    <div class="hidden-xs hidden-sm col-md-12">
         
-        <div id="main" class="row col-md-8 clearfix" role="main">
+        <?php
+            //$bg_img = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'full' );
+            $gem_bg_img = '';
+
+//            if ($party_img == "Spring") {
+//                
+//                $bg_img = get_field('spring_bg_img', 'option');
+//                
+//            }
+//            if ($party_img == "Default") {
+//                
+//                $bg_img = get_field('default_bg_img', 'option');
+//                
+//            }
+//            if ($party_img == "Collegiate") {
+//                
+//                $bg_img = get_field('collegiate_bg_img', 'option');
+//                
+//            }
+
             
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            //echo $party_img;
+            //print_r($bg_img);
+
+// author meta
+    //$author_store = get_the_author_meta( 'user_nicename' );
+ 
+$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+
+$auth_meta = get_the_author_meta('ID');
+
+//$gem_profile_img = get_avatar( $curauth->ID );
+$gem_profile_img = get_field( 'photo' );
+
+        ?>
+        
+        <div class="party-img" style="background: url('<?php echo $bg_img['url']; ?>') top right no-repeat;height:100px;">
+
+            <div class="col-md-offset-1">
+                
+                <div class="pull-left"><?php echo $gem_profile_img; ?></div>
+                
+                <h1 class="party-title pull-left">Shop <?php echo $curauth->display_name; ?></h1>
             
-            <?php the_title(); ?>
-            
-        </div>	<!-- end #main -->			
-        <div id="sidebar" class="row col-md-4 col-xs-12">	
-            <div class="pull-left">
-                <img src="<?php echo of_get_option( 'gem_icon', 'no entry' ); ?>">
             </div>
-            <?php get_sidebar('sidebar2'); // sidebar 2 ?>			
+            
+        </div>
+              
+    </div>
+</div>
+
+<div id="page-wrap" class="row">
+    
+    <div class="col-md-12">
+    
+    <div id="page-left" class="col-md-7 col-md-offset-1">
+        
+            
+            
+        
+            
+        
+        <!-- start shopping items -->
+        
+        <?php echo do_shortcode('[recent_products per_page="12" columns="4"]'); ?>
+        
+        <!-- end shopping items -->
+            
+        </div>	<!-- end #main -->	
+        
+        <div id="sidebar" class="col-md-3">
+                    
+            <?php dynamic_sidebar('products-main'); // Shop sidebar ?>			
                         
         </div> <!-- end #sidebar -->
-            
-            <?php endwhile; ?>	
-            
-            <?php else : ?>
-            
-            <?php endif; ?>
+        
+        
     
-    </div> <!-- end #content -->
+    </div> 
+    
+</div> <!-- end page wrap -->
+
+<?php 
+
+    $current_user = wp_get_current_user();
+    $party_author = get_the_author_meta('ID');
+
+if ( $current_user->ID == $party_author ) {
+
+    get_template_part( 'section', 'gem-shop' ); 
+
+}
+
+?>
+
+<?php get_template_part( 'section', 'links' ); ?>
+
 
 <?php get_footer(); ?>
--- section-gem-shop.php
+++ section-gem-shop.php
@@ -2,35 +2,29 @@
 		// pull meta for each post
 		$post_id = get_the_ID();
 		$permalink = get_permalink();
-		$party_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail', false, '');
-		$party_img_url = $party_img[0];
-		$party_name = get_the_title();
-        $gem_url = get_field( "party_url" );
+		//$party_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail', false, '');
+		//$party_img_url = $party_img[0];
+		//$party_name = get_the_title();
         $gem_id = get_the_author_meta('display_name');
         $gem_fname = get_the_author_meta('first_name');
         $gem_lname = get_the_author_meta('last_name');
         $gem_name = $gem_fname . ' ' . $gem_lname;
 		
-		$edit_link_url = get_edit_post_link($post->ID);
-		$edit_link = '<span class="edit-pencil-link"><a href="' . $edit_link_url . '"><i class="fa fa-pencil"></i></a></span>';
-		
-		// set variables
-        if( empty( $business_img_url ) ) {
-        	$business_img_url = "/wp-content/themes/Gem/images/gem-icon-pink-25.png";
-				};
-				
-		if( empty( $edit_link_url ) ) {
-        	$edit_link = "";
-				};
         // Author info
         $author_url = get_the_author_meta('user_url');
-        $author_fname = get_the_author_meta('first_name'); 
+        $author_fname = get_the_author_meta('first_name');
+
+        // user info
+        $user_in = get_currentuserinfo();
+        $user_info = get_userdata( $author_url);
 
         // get message from options tab
         $email_tmp_party = get_field('email_tmp', 'option');
         $party_invite_headline = get_field('party_invite_headline', 'option');
         $party_invite_offer = get_field('party_invite_offer', 'option');
         $party_invite_button = get_field('party_invite_button', 'option');
+
+        //$author_link = get_author_posts_url( 1 );
         
 
 //HTML
@@ -44,17 +38,27 @@
     
     <div class="col-md-10 col-md-offset-1">
     
-    <div class="hrule"></div>    
-     <?php echo $author_fname; ?>'s Shop URL = <?php echo $author_url; ?>  
+    <div class="hrule"></div>
+        
+        
+        
+<?php  
+    if ($user_in->user_nicename == $author_url) { 
+      echo '<a href="/' . $user_info->user_login . '/?mygem=' . $user_info->display_name . '">www.rockdarling.com/' . $user_info->user_login . '</a></br>';
+        
+    echo 'User roles: ' . implode(', ', $user_info->roles) . '</br>';
+    echo 'User ID: ' . $user_info->ID . '</br>';
+    echo 'User Display name: ' . $user_info->display_name . '</br>';
+        
+        //print_r($user_info);
+    }
+?>
+        
     <div class="hrule"></div>
         
 <h4><?php echo $gem_fname; ?>, Market your party by sending the following email to your clients</h4>
 <div class="hrule"></div>
         
-        <?php
-            echo '<h3>Party Link:</br></h3>';
-            echo '<pre>' . $permalink . '?mygem=' . $gem_id . '</pre>';
-        ?>
         <div class="hrule"></div>
         <div class="code-preview">
             
@@ -62,7 +66,7 @@
         <p><?php echo $email_tmp_party; ?></p>
         <h5><?php echo $party_invite_offer; ?></h5></br></br>
         
-        <p><a style="padding:8px 16px; color:white; background-color:#00b2a9; text-decoration:none;" href="<?php echo $permalink . '?mygem=' . $gem_id; ?>" target="_blank"><?php echo $party_invite_button; ?> <?php echo $party_name; ?></a></p></br></br>
+        <p><a style="padding:8px 16px; color:white; background-color:#00b2a9; text-decoration:none;" href="<?php echo $user_info->user_login . '?mygem=' . $user_info->display_name; ?>" target="_blank"><?php echo $user_info->display_name; ?></a></p></br></br>
         
         <h4>Your Gem,</h4>
         <h5 class="script lg"><?php echo $gem_name; ?></h5>
